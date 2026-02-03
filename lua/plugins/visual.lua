cd1dcc6ce53af339d6b9e3e8f90eeeb3cf332210
C-Birkett <conanbirkett@gmail.com>
2026-02-03 15:52:13 +0000

    Fix lualine add opencode thing (no working icons atm)

--- lua/plugins/visual.lua
+++ lua/plugins/visual.lua
@@ -1,35 +1,45 @@
+local globals = require("globals")
+
 return {
     {
         'nvim-lualine/lualine.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        dependencies = {
+            'nvim-tree/nvim-web-devicons',
+            globals.USE_COPILOT and 'NickvanDyke/opencode.nvim' or nil,
+        },
         config = function()
+            local sections = {
+                lualine_b = {
+                    "branch",
+                    "diff",
+                    {
+                        -- fix bad diagnostics icons
+                        "diagnostics",
+                        symbols = { error = '', warn = '', info = 'ⓘ', hint = '💡︎' },
+                    }
+                },
+                lualine_x = {
+                    'encoding',
+                    'filetype',
+                    {
+                        'fileformat',
+                        symbols = {
+                            unix = " LF",
+                            dos = " CRLF",
+                            mac = " CR",
+                        }
+                    },
+                    globals.USE_COPILOT and require("opencode").statusline or nil,
+                }
+            }
+
             require("lualine").setup({
                 options = {
                     theme = "nord",
                     icons_enabled = true,
                 },
                 extensions = {"neo-tree", "mason", "lazy"},
-                -- fix bad diagnostics icons
-                sections = {
-                    lualine_b = {
-                        "branch",
-                        "diff",
-                        {
-                            "diagnostics",
-                            symbols = { error = '', warn = '', info = 'ⓘ', hint = '💡︎' },
-                        }
-                    },
-                    lualine_y = {
-                        {
-                            "fileformat",
-                            symbols = {
-                                unix = "LF",
-                                dos = "CRLF",
-                                mac = "CR",
-                            }
-                        }
-                    }
-                }
+                sections = sections
             })
         end
     },
